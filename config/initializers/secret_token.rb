a950f2dc72449cc77a931f25ac936cda30ce35b5
tranbichcuong <tranbichcuong@gmail.com>
2012-10-16 15:27:35 +0700

    finish bai3_1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bai31::Application.config.secret_token = 'f2e77a0846a1da70559edc15bcb961a8d9bb9e2815afb549d8f03458f0f7dac86757859e8d4736d18f5ab20e0d74fcfcd5fff5ab9660db19be9e9ca4107fb7bb'
